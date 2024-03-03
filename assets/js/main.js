8f63f3b8eec5ef027fa7cc8d0cb3ec0dab3e5ffd
rakibuli55 <islamrakibul8383@gmail.com>
2024-03-03 19:16:51 +0600

    css modify

--- assets/js/main.js
+++ assets/js/main.js
@@ -354,33 +354,34 @@
             function categoryhandle() {
                 var categories = document.querySelectorAll('.contact--popup--wrapper .pop--interest--group label');
                 var categoriesSpan = document.querySelectorAll('.contact--popup--wrapper .pop--interest--group label span');
-                console.log(categories);
             
-                categories.forEach((categori) => {
-                    console.log(categori);
-                    categori.addEventListener('click', (e) => {
-                        var current = e.target;
-                        if(current.classList.contains('active')){
-                            current.classList.remove('active'); 
-                        }else{
-                            current.classList.add('active');
-                        }
-                        
+                if(categories){
+                    categories.forEach((categori) => {
+                        console.log(categori);
+                        categori.addEventListener('click', (e) => {
+                            var current = e.target;
+                            if(current.classList.contains('active')){
+                                current.classList.remove('active'); 
+                            }else{
+                                current.classList.add('active');
+                            }
+                            
+                        });
                     });
-                });
-                categoriesSpan.forEach((categorispan) => {
-                    
-                    categorispan.addEventListener('click', (e) => {
-                        var current = e.target.parentElement;
-                        console.log(current)
-                        if(current.classList.contains('active')){
-                            current.classList.remove('active'); 
-                        }else{
-                            current.classList.add('active');
-                        }
+                    categoriesSpan.forEach((categorispan) => {
                         
+                        categorispan.addEventListener('click', (e) => {
+                            var current = e.target.parentElement;
+                            console.log(current  )
+                            if(current.classList.contains('active')){
+                                current.classList.remove('active'); 
+                            }else{
+                                current.classList.add('active');
+                            }
+                            
+                        });
                     });
-                });
+                }
             }
             
             categoryhandle();
